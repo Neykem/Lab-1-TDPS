a71274010d7a17087cbe8c1e89785f037246253b
Neykem <33416493+Neykem@users.noreply.github.com>
2020-03-17 16:19:33 +0300

    AnimationTestRel

--- TDPS - Water Spill Machine S1/TDPS - Water Spill Machine S1/MainWindow.xaml.cs
+++ TDPS - Water Spill Machine S1/TDPS - Water Spill Machine S1/MainWindow.xaml.cs
@@ -16,30 +16,63 @@ using System.Windows.Media.Animation;
 
 namespace TDPS___Water_Spill_Machine_S1
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
+    static public class GlobalVar
+    {
+        static public bool button_animation_switch_stat = true;
+    }
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
-            InitializeComponent();
+            // Animation Section
+            #region 
+            this.InitializeComponent();
+
             transform.BeginAnimation(RotateTransform.AngleProperty,
         new DoubleAnimation
         {
             From = 0,
-            To = 14000,
+            To = 16000,
             Duration = TimeSpan.FromSeconds(360)
         });
             transform2.BeginAnimation(RotateTransform.AngleProperty,
         new DoubleAnimation
         {
-            From = 60,
-            To = 14060,
+            From = 80,
+            To = 16080,
             Duration = TimeSpan.FromSeconds(360)
-});
+        });
+            #endregion
+            System.Windows.Threading.DispatcherTimer timer = new System.Windows.Threading.DispatcherTimer();
+            timer.Tick += new EventHandler(Timer_Tick);
+            timer.Interval = new TimeSpan(0, 0, 4);
+            timer.Start();
         }
-            private void Button_Click(object sender, RoutedEventArgs e)
+
+        private void GetWater(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            if (GlobalVar.button_animation_switch_stat == false)
+            {
+                Storyboard_1.Begin();
+                GlobalVar.button_animation_switch_stat = true;
+            }
+            else
+            {
+                Storyboard_2.Begin();
+                GlobalVar.button_animation_switch_stat = false;
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void ButtonDoubleClickClose(object sender, MouseButtonEventArgs e)
         {
             this.Close();
         }
--- TDPS - Water Spill Machine S1/TDPS - Water Spill Machine S1/obj/Debug/TDPS - Water Spill Machine S1_Content.g.i.cs
+++ TDPS - Water Spill Machine S1/TDPS - Water Spill Machine S1/obj/Debug/TDPS - Water Spill Machine S1_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("res/4.png")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("res/vint.png")]
 
 
